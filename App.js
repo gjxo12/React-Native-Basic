f40337e927b9858bfb5ddf2cf10d703303ce3b66
HTK <vhxpffltm@naver.com>
2022-04-17 21:36:38 +0900

    chore: fix app.js and using navigate stack

--- App.js
+++ App.js
@@ -1,53 +1,24 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import AppStack from './navigation';
 import Home from './screen/home';
+import Quiz from './screen/quiz';
+import Result from './screen/result';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppStack></AppStack>
+    </NavigationContainer>
+  );
+};
 
-  render() {
-    return (
-      <View style={this.style.container}>
-        <Home />
-        <Text> Sample Quiz </Text>
+export default App;
 
-      </View>
-    );
-  }
-
-  style = StyleSheet.create({
-    container:{
-      paddingTop:40,
-      paddingHorizontal:16,
-    }
-  })
-
-}
-
-
-// import react from 'react';
-// import React, { Component } from 'react';
-// import { View, Text ,StyleSheet} from 'react-native';
-
-// const App = () =>{
-//   return (
-//     <View style={style.container}>
-//       <Text> Sampe Quiz </Text>
-//     </View>
-//   );
-// };
-
-// export default App;
-
-// const style = StyleSheet.create({
-//   container:{
-//     paddingTop:40,
-//     paddingHorizontal: 16,
-//   }
-// });
-
-
+const style = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    paddingHorizontal: 16,
+  },
+});
---
+++ navigation/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screen/home';
+import Quiz from '../screen/quiz';
+import Result from '../screen/result';
+
+const Stack = createStackNavigator();
+
+function AppStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Quiz"
+        component={Quiz}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Result"
+        component={Result}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default AppStack;
